b9d756cf75ba3be6a98074b5295338349bb59f35
Sacha Elkaim <sachaelkaim87@gmail.com>
2020-10-05 01:40:16 -0400

    springboot

---
+++ back-end/src/test/java/soen343/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package soen343.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
