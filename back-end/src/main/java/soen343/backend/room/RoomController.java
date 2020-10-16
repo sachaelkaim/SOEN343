396e20b7d6fe0cb7c5bf95007dd66999a0a936d8
CamilBouzidi <camilbouzidi@hotmail.com>
2020-10-16 16:13:54 -0400

    Removed House API, Updated Room API
    Due to an error in my other branch, I will be merging this one instead. It adds a Room API closer to the algorithm we have in mind.

---
+++ back-end/src/main/java/soen343/backend/room/RoomController.java
@@ -0,0 +1,44 @@
+package soen343.backend.room;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("http://localhost:3000") //to unblock request to/from react
+@RequestMapping("api/")
+public class RoomController {
+
+    @Autowired
+    private RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/rooms")
+    public Iterable<Room> getAllRooms() {
+        return roomService.getAllRooms();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/rooms/{name}")
+    public Optional<Room> getRoom(@PathVariable String name){
+        return roomService.getRoom(name);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/rooms")
+    public void addRoom(@RequestBody Room room){
+        roomService.addRoom(room);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/rooms/{name}")
+    public void editRoom(@RequestBody Room room, @PathVariable String name) {
+        roomService.editRoom(name, room);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/rooms/{name}")
+    public void deleteRoom(@PathVariable String name) {
+         roomService.deleteRoom(name);
+    }
+}
