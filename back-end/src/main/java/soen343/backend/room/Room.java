5bf86846e30263acb531cab0ac3e2c15a88d022c
Lorenzo001 <34139780+Lorenzo001@users.noreply.github.com>
2020-10-17 18:31:37 -0400

    Update Room.java
    Added color function

--- back-end/src/main/java/soen343/backend/room/Room.java
+++ back-end/src/main/java/soen343/backend/room/Room.java
@@ -7,23 +7,32 @@ import javax.persistence.Id;
 
 @Entity
 public class Room {
-	enum WindowState {OPENED,CLOSED,BLOCKED,TRANSITIONING}
-	enum DoorState {LOCKED, UNLOCKED, ALERT}
+	enum WindowState {
+		OPENED, CLOSED, BLOCKED, TRANSITIONING
+	}
 
-    @Id
-    private String name;
-    private WindowState windowState;
-    private DoorState doorState;
-    private boolean lightOn = true;
-    private double temperature;
-    
-    
+	enum DoorState {
+		LOCKED, UNLOCKED, ALERT
+	}
 
-    public Room() {
+	enum Color {
+		YELLOW, GREEN, RED
+	}
+
+	@Id
+	private String name;
+	private WindowState windowState;
+	private DoorState doorState;
+	private boolean lightOn = true;
+	private double temperature;
+	private Color color;
+
+	public Room() {
 		super();
 	}
 
-	public Room(int id, String name, WindowState windowState, DoorState doorState, boolean lightOn, double temperature) {
+	public Room(int id, String name, WindowState windowState, DoorState doorState, boolean lightOn,
+			double temperature) {
 		super();
 		this.name = name;
 		this.windowState = windowState;
@@ -63,7 +72,7 @@ public class Room {
 	public void setLightOn(boolean lightOn) {
 		this.lightOn = lightOn;
 	}
-	
+
 	public double getTemperature() {
 		return temperature;
 	}
@@ -72,5 +81,40 @@ public class Room {
 		this.temperature = temperature;
 	}
 
-    
+	public Color getDoorColor(Room room) {
+		color = Color.RED;
+		switch (room.getDoorState()) {
+		case UNLOCKED:
+			color = Color.GREEN;
+			break;
+		case LOCKED:
+			color = Color.RED;
+			break;
+		case ALERT:
+			color = Color.YELLOW;
+			break;
+		default:
+			break;
+		}
+		return color;
+	}
+
+	public Color getWindowColor(Room room) {
+		color = Color.RED;
+		switch (room.getWindowState()) {
+		case OPENED:
+			color = Color.GREEN;
+			break;
+		case BLOCKED:
+			color = Color.YELLOW;
+			break;
+		case CLOSED:
+			color = Color.RED;
+			break;
+		default:
+			break;
+		}
+		return color;
+	}
+
 }
