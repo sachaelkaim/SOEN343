3551ffd114035de8c8013ebe6276287c47f24542
Lorenzo001 <34139780+Lorenzo001@users.noreply.github.com>
2020-11-06 13:47:59 -0500

    added permissions
    Give the list of the user's permissions based on the access of each user profile and the location of the user

--- front-end/src/components/modules/SHS.js
+++ front-end/src/components/modules/SHS.js
@@ -6,6 +6,13 @@ import { UserContext } from "../UserProvider";
 import { LayoutContext } from "../LayoutProvider";
 import { AllUsersContext } from "../AllUsersProvider";
 
+//Permissions
+const Permissions={
+  0:["open/close windows","unlock doors","open/close garage"," turn on/off lights"],
+  1:["turn on/off lights","open/close windows"],
+  2:["turn on/off lights","open/close windows"],
+  3:["permissions is revoked"]
+}
 // SHS module
 const SHS = () => {
   const [formData, setFormData] = useState([]);
@@ -118,6 +125,25 @@ const SHS = () => {
     setNewTemperature({ ...newTemperature, [e.target.name]: e.target.value });
   };
 
+  // update permissions
+  const getPermissions=(privilege,location)=>{
+
+    if(privilege==0)
+      return Permissions[privilege].join(",");
+    else{
+      if(location=="Outside")
+      {
+        return "permissions is revoked"
+      }
+      if(privilege==1)
+        return Permissions[privilege].join(",");
+      if(privilege==2)
+        return Permissions[privilege].join(",");
+      if(privilege==3)
+        return Permissions[privilege].join(",");
+    }  
+  }
+
   // update outdoor temperature
   const updateOutdoorTemperature = async (e) => {
     console.log(newTemperature.id)
@@ -134,6 +160,7 @@ const SHS = () => {
       .catch((err) => console.log("Error", err));
     getRooms();
   };
+
 
   return (
     <>
@@ -247,6 +274,7 @@ const SHS = () => {
               &nbsp;
               {item.privilege}
               &nbsp;
+              <div>Permissions:({getPermissions(item.privilege,item.location)})</div>
               <Button
                 variant="light"
                 style={{ fontSize: "10px" }}
